5de45e08542b1b07e5c53ccf3192c411eed376ed
Mohammad Abu Sakib <mabusakib@gmail.com>
2018-09-06 21:23:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,10 @@
 
 #****************************************************************
 
-#from dropbox.exceptions import AuthError
-from shutil import copyfile
 from subprocess import call
 import os
 import re
-import sys
 
-#TOKEN = ' '
 #directory where files will uploaded to Dropbox
 dirname = '/home/Documents/'    
 
@@ -32,38 +28,3 @@ for files in localfiles:
         if os.path.isdir(files):    #if exists, check if it's a file or a dir
             no_of_dirs += 1
             call(['./zipit.sh', re.sub(r'[\s+]', r'\ ', files)]) # in case path contains whitespaces
-
-#create an instance of a Dropbox class, which can make requests to the API.
-#dbox_obj = dropbox.Dropbox(fiload. timeout=None)
-
-'''
-#check for an access token
-if (len(TOKEN) == 0):
-    sys.exit("ERROR: Looks like you didn't add your access token. "
-        "Kindly put your token in line 17 of the source code.")
-
-#check that the access token is valid
-try:
-    dbox_obj.users_get_current_account()
-except AuthError as err:
-    sys.exit("ERROR: Invalid access token; try re-generating an access token")
-'''
-
-#upload.backup(dbox_obj)
-#print("Uploading done!")
-
-
-#*************************************************************
-'''
-#user-defined function for extracting file extension, if there is any
-def get_exton(path_to_local_file):
-    file_base = os.path.basename(path_to_local_file)
-    start = len(file_base) - 1
-    while start:
-        if file_base[start] == '.':
-            exton = file_base[start:len(file_base)-1]
-            break
-        start = start - 1
-    return exton
-'''
-#**************************************************************
